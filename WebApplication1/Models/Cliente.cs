9dda31052aa8f98ed677a6f74c466160dacafe77
Edwin Saa <edwin.saa@udla.edu.ec>
2025-05-04 01:55:04 -0500

    Client model change

--- WebApplication1/Models/Cliente.cs
+++ WebApplication1/Models/Cliente.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
 
 namespace WebApplication1.Models
 {
@@ -8,13 +9,22 @@ namespace WebApplication1.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string NombreCompleto { get; set; }
+        [Required]
+        [StringLength(50)]
         public string ApellidoCompleto { get; set; }
+        [Required]
+        [EmailAddress]
         public string Correo { get; set; }
+        [Required]
+        [Phone]
         public string Telefono { get; set; }
-        [ForeignKey("Direccion")]
+        [StringLength(200)]
         public string Direccion { get; set; }
+        [Required]
         public string MetodoPago { get; set; }
-        
+
     }
 }
