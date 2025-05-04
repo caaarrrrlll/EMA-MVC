414a824f388ad95b7ac43dc720fde8c449915748
Ludicard <luckydicey38@gmail.com>
2025-05-03 23:50:19 -0500

    Actualizado Modelo OrdenTrabajo

--- WebApplication1/Models/OrdenTrabajo.cs
+++ WebApplication1/Models/OrdenTrabajo.cs
@@ -1,6 +1,37 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class OrdenTrabajo
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string NombreEquipo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CodigoEquipo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TipoMantenimiento { get; set; }
+
+        [Required]
+        public DateTime FechaIngreso { get; set; }
+
+        public DateTime? FechaEntrega { get; set; }
+
+        [StringLength(500)]
+        public string DescripcionProblema { get; set; }
+
+        [StringLength(500)]
+        public string Observaciones { get; set; }
+
+        [StringLength(20)]
+        public string Estado { get; set; }
+
     }
 }
