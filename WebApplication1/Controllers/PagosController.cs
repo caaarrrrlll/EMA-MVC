02797067392545e2df40e95c5e01edab76e09423
caaarrrrlll <carlosmoreta140@gmail.com>
2025-05-04 00:46:30 -0500

    Creacion del controlador pagos, asi como las migraciones y edicion del app.json

---
+++ WebApplication1/Controllers/PagosController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PagosController : Controller
+    {
+        private readonly ServerEMA _context;
+
+        public PagosController(ServerEMA context)
+        {
+            _context = context;
+        }
+
+        // GET: Pagos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pagos.ToListAsync());
+        }
+
+        // GET: Pagos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pagos = await _context.Pagos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pagos == null)
+            {
+                return NotFound();
+            }
+
+            return View(pagos);
+        }
+
+        // GET: Pagos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pagos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,VentaId,Monto,FechaPago,EntidadExterna,MetodoPago,DetallesCuenta")] Pagos pagos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pagos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pagos);
+        }
+
+        // GET: Pagos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pagos = await _context.Pagos.FindAsync(id);
+            if (pagos == null)
+            {
+                return NotFound();
+            }
+            return View(pagos);
+        }
+
+        // POST: Pagos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,VentaId,Monto,FechaPago,EntidadExterna,MetodoPago,DetallesCuenta")] Pagos pagos)
+        {
+            if (id != pagos.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pagos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PagosExists(pagos.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pagos);
+        }
+
+        // GET: Pagos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pagos = await _context.Pagos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pagos == null)
+            {
+                return NotFound();
+            }
+
+            return View(pagos);
+        }
+
+        // POST: Pagos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pagos = await _context.Pagos.FindAsync(id);
+            if (pagos != null)
+            {
+                _context.Pagos.Remove(pagos);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PagosExists(int id)
+        {
+            return _context.Pagos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApplication1/Migrations/20250504054525_Migracion Inicial.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Serial = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaAdquisicion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaBaja = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrdenTrabajo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreEquipo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CodigoEquipo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TipoMantenimiento = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaIngreso = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaEntrega = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DescripcionProblema = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Observaciones = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdenTrabajo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pagos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VentaId = table.Column<int>(type: "int", maxLength: 50, nullable: false),
+                    Monto = table.Column<decimal>(type: "decimal(18,2)", maxLength: 50, nullable: false),
+                    FechaPago = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EntidadExterna = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MetodoPago = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DetallesCuenta = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    OrdenTrabajoId = table.Column<int>(type: "int", nullable: false),
+                    EmpresaId = table.Column<int>(type: "int", nullable: false),
+                    EquipoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pagos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pagos_Empresa_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "Empresa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pagos_Equipo_EquipoId",
+                        column: x => x.EquipoId,
+                        principalTable: "Equipo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pagos_OrdenTrabajo_OrdenTrabajoId",
+                        column: x => x.OrdenTrabajoId,
+                        principalTable: "OrdenTrabajo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagos_EmpresaId",
+                table: "Pagos",
+                column: "EmpresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagos_EquipoId",
+                table: "Pagos",
+                column: "EquipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagos_OrdenTrabajoId",
+                table: "Pagos",
+                column: "OrdenTrabajoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pagos");
+
+            migrationBuilder.DropTable(
+                name: "Empresa");
+
+            migrationBuilder.DropTable(
+                name: "Equipo");
+
+            migrationBuilder.DropTable(
+                name: "OrdenTrabajo");
+        }
+    }
+}
