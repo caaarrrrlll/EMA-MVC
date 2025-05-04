a40f740f29cdca9af0cba58d7e1f23621846f7aa
Edwin Saa <edwin.saa@udla.edu.ec>
2025-05-03 23:32:30 -0500

    Admin Model structure

--- WebApplication1/Models/Administrador.cs
+++ WebApplication1/Models/Administrador.cs
@@ -2,5 +2,19 @@
 {
     public class Administrador
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Contrasena { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        // Constructor
+        public Administrador()
+        {
+            FechaCreacion = DateTime.Now;
+            FechaModificacion = DateTime.Now;
+        }
     }
 }
