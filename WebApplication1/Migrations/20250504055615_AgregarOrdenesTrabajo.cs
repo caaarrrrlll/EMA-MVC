9533436262d44e936de609faa7ae49ede6f62250
Ludicard <luckydicey38@gmail.com>
2025-05-04 00:56:46 -0500

    Agregada Migracion AgregarOrdenesTrabajo

---
+++ WebApplication1/Migrations/20250504055615_AgregarOrdenesTrabajo.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarOrdenesTrabajo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pagos_Empresa_EmpresaId",
+                table: "Pagos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pagos_Equipo_EquipoId",
+                table: "Pagos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pagos_OrdenTrabajo_OrdenTrabajoId",
+                table: "Pagos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pagos_Empresa_EmpresaId",
+                table: "Pagos",
+                column: "EmpresaId",
+                principalTable: "Empresa",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pagos_Equipo_EquipoId",
+                table: "Pagos",
+                column: "EquipoId",
+                principalTable: "Equipo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pagos_OrdenTrabajo_OrdenTrabajoId",
+                table: "Pagos",
+                column: "OrdenTrabajoId",
+                principalTable: "OrdenTrabajo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pagos_Empresa_EmpresaId",
+                table: "Pagos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pagos_Equipo_EquipoId",
+                table: "Pagos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pagos_OrdenTrabajo_OrdenTrabajoId",
+                table: "Pagos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pagos_Empresa_EmpresaId",
+                table: "Pagos",
+                column: "EmpresaId",
+                principalTable: "Empresa",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pagos_Equipo_EquipoId",
+                table: "Pagos",
+                column: "EquipoId",
+                principalTable: "Equipo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pagos_OrdenTrabajo_OrdenTrabajoId",
+                table: "Pagos",
+                column: "OrdenTrabajoId",
+                principalTable: "OrdenTrabajo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
